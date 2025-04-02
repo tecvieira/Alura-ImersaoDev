436cb7f3633f06d1facc52fd5c23a9a638a525e1
Robson Vieira <tecvander.vieira@gmail.com>
2025-04-02 08:37:29 -0300

    com fundo final fantasy
    sem logo

--- aula002dev.js
+++ aula002dev.js
@@ -1,43 +1,44 @@
-idade = prompt("Qual sua idade?");
-if (idade < 18) {
-  alert("Você não pode jogar jokenpô");
-}else{
-escolhaJogador = prompt("1-Pedra, 2-Papel ou 3-Tesoura")
-escolhaComputador = Math.floor(Math.random() * 3 + 1);
+function jogar(){
+    idade = prompt("Qual sua idade?");
+    if (idade < 18) {
+    alert("Você não pode jogar jokenpô");
+    }else{
+    escolhaJogador = prompt("1-Pedra, 2-Papel ou 3-Tesoura")
+    escolhaComputador = Math.floor(Math.random() * 3 + 1);
 
-// Jogador e computador escolheram a mesma coisa
-if(escolhaJogador == escolhaComputador){
-alert("Empate o computador escolheu:" + escolhaComputador)
-}
+    // Jogador e computador escolheram a mesma coisa
+    if(escolhaJogador == escolhaComputador){
+    alert("Empate o computador escolheu:" + escolhaComputador)
+    }
 
-//-----------------------JOGADA 1 PEDRA------------------ 
-if(escolhaJogador == 1){
-if(escolhaComputador == 2){
-    alert("Jogador PERDEU! O computador jogou: " + escolhaComputador + "- PAPEL" )
-}
-if(escolhaComputador == 3){
-    alert("O computador PERDEU! escolheu tesoura: " + escolhaComputador + "PAPEL")
-}
-}
-//----------JOGADA 2 PAPEL--------
-if(escolhaJogador == 2){
-if(escolhaComputador == 1){
-    alert("Jogador VENCEU, escolheu PAPEL e o computador: PEDRA"  )
-}
-if(escolhaComputador == 3){
-    alert("O computador VENCEU escolheu tesoura e voçê PAPEL")
-}
-}
+    //-----------------------JOGADA 1 PEDRA------------------ 
+    if(escolhaJogador == 1){
+    if(escolhaComputador == 2){
+        alert("Jogador PERDEU! O computador jogou: " + escolhaComputador + "- PAPEL" )
+    }
+    if(escolhaComputador == 3){
+        alert("O computador PERDEU! escolheu tesoura: " + escolhaComputador + "PAPEL")
+    }
+    }
+    //----------JOGADA 2 PAPEL--------
+    if(escolhaJogador == 2){
+    if(escolhaComputador == 1){
+        alert("Jogador VENCEU, escolheu PAPEL e o computador: PEDRA"  )
+    }
+    if(escolhaComputador == 3){
+        alert("O computador VENCEU escolheu tesoura e voçê PAPEL")
+    }
+    }
 
-//-------------JOGADA 3 TESOURA---------
-if(escolhaJogador == 3){
-if(escolhaComputador == 1){
-    alert("Jogador PERDEU, escolheu TESOURA e o computador: PEDRA"  )
+    //-------------JOGADA 3 TESOURA---------
+    if(escolhaJogador == 3){
+    if(escolhaComputador == 1){
+        alert("Jogador PERDEU, escolheu TESOURA e o computador: PEDRA"  )
+    }
+    if(escolhaComputador == 2){
+        alert("O computador PERDEU escolheu PAPEL e voçê TESOURA")
+    }
+    }
+    }
 }
-if(escolhaComputador == 2){
-    alert("O computador PERDEU escolheu PAPEL e voçê TESOURA")
-}
-}
-}
-
 
